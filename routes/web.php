584a74a52605d0d94aa7ebf934cc3dbaaf7282ad
Jan Hansen Tiu <tiujanhansen@gmail.com>
2025-11-25 20:45:36 +0800

    [+] Preparing repo for use

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+//EVERYTHING MUST BE AUTH! NO GUESTS SINCE THIS IS ONLY FOR UC PEOPLE
+Route::middleware('auth')->group(function()
+{
+    // Route::get('/', [Controller::class, 'index'])->name('home');
+});
+
+require __DIR__.'/auth.php';
